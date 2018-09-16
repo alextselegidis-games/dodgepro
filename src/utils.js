d77132f6cf55fba3014e553ee55a09ff16db5fc5
alext <alextselegidis@gmail.com>
2018-09-16 10:40:12 +0200

    Moved cordova initialization out of the main.js file

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,30 @@
 export const centerGameObjects = (objects) => {
-  objects.forEach(function (object) {
-    object.anchor.setTo(0.5)
-  })
-}
+    objects.forEach(function (object) {
+        object.anchor.setTo(0.5);
+    });
+};
+
+export const initializeCordova = () => {
+    const app = {
+        initialize: function () {
+            document.addEventListener(
+                'deviceready',
+                this.onDeviceReady.bind(this),
+                false
+            )
+        },
+
+        onDeviceReady: function () {
+            this.receivedEvent('deviceready')
+
+            // When the device is ready, start Phaser Boot state.
+            window.game.state.start('Boot')
+        },
+
+        receivedEvent: function (id) {
+            console.log('Received Event: ' + id)
+        }
+    };
+
+    app.initialize();
+};
