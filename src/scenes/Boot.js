ff1c410f1f03aee1102da294d65bc1b55e4be58a
alext <alextselegidis@gmail.com>
2018-10-12 15:00:57 +0200

    Offline font loading fallback.

--- src/scenes/Boot.js
+++ src/scenes/Boot.js
@@ -27,15 +27,17 @@ export default class extends Phaser.Scene {
         this.fontsReady = false;
         this.fontsLoaded = this.fontsLoaded.bind(this);
         this.add.text(100, 100, 'loading fonts...');
-        this.load.image('loaderBg', './assets/images/loader-bg.png');
-        this.load.image('loaderBar', './assets/images/loader-bar.png');
 
         WebFont.load({
             google: {
                 families: ['Rajdhani']
             },
             active: this.fontsLoaded
-        })
+        });
+
+        setTimeout(() => {
+            this.fontsReady = true; // fallback in case fonts are not loading (offline)
+        }, 1000);
     }
 
     update() {
